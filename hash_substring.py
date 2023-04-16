b82a9d408b5fb91a1bdaa365da8dd8f653f9d458
valters221rdb215 <125762443+valters221rdb215@users.noreply.github.com>
2023-04-16 21:15:50 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -20,16 +20,14 @@ def read_input():
 
 
 def print_occurrences(output):
-
     print(' '.join(map(str, output)))
 
-     
 
 def get_occurrences(pattern, text):
     patternLength = len(pattern)
     textLength = len(text)
-    patternHash= sum([ord(c) for c in pattern])
-    textHash = sum([ord(c) for c in text[:patternLength])
+    patternHash = sum([ord(c) for c in pattern])
+    textHash = sum([ord(c) for c in text[:patternLength]])
 
     for i in range(0, textLength - patternLength + 1):
         if patternHash == textHash and pattern == text[i:i+patternLength]:
