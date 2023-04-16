f8051ab21b33b8e5a6af2396c35583fd19641538
valters221rdb215 <125762443+valters221rdb215@users.noreply.github.com>
2023-04-16 17:14:00 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,4 +1,3 @@
-# python3
 def read_input():
     mode = input().strip()
 
@@ -20,33 +19,24 @@ def read_input():
     return pattern, text
 
 
-
-
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
+
     print(' '.join(map(str, output)))
-    
-     
-        
-        
+
+
 def get_occurrences(pattern, text):
     pattern_len = len(pattern)
     text_len = len(text)
     pattern_hash = sum([ord(c) for c in pattern])
     text_hash = sum([ord(c) for c in text[:pattern_len]])
 
-    for i in range(1, text_len - pattern_len + 2):
-        if pattern_hash == text_hash and pattern == text[i-1:i-1+pattern_len]:
-            yield i-1
+    for i in range(0, text_len - pattern_len + 1):
+        if pattern_hash == text_hash and pattern == text[i:i+pattern_len]:
+            yield i
 
-        if i < text_len - pattern_len + 1:
-            text_hash -= ord(text[i-1])
-            text_hash += ord(text[i+pattern_len-1]))
-
-
-    return list(get_occurrences(pattern, text))
-
-
+        if i < text_len - pattern_len:
+            text_hash -= ord(text[i])
+            text_hash += ord(text[i+pattern_len])
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
